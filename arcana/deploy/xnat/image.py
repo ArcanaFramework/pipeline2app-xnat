162331f8f72a7f6dc3d8df8c6ed96218dfcc4364
Tom Close <tom.g.close@gmail.com>
2022-12-19 19:58:57 +1100

    getting build system to work

--- arcana/deploy/xnat/image.py
+++ arcana/deploy/xnat/image.py
@@ -7,12 +7,12 @@ from neurodocker.reproenv import DockerRenderer
 from arcana.data.stores.xnat import XnatViaCS
 from arcana.core.utils.serialize import ClassResolver, ObjectConverter
 from arcana.core.data.store import DataStore
-from arcana.core.deploy.image import CommandImage
+from arcana.core.deploy.image import AppImage
 from .command import XnatCSCommand
 
 
 @attrs.define(kw_only=True)
-class XnatCSImage(CommandImage):
+class XnatCSImage(AppImage):
 
     command: XnatCSCommand = attrs.field(
         converter=ObjectConverter(
