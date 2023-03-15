e9781fbf375f51d99e33bbccad852c427a31f284
Tom Close <tom.g.close@gmail.com>
2023-03-15 16:09:54 +1100

    debugged CS put

--- arcana/xnat/deploy/command.py
+++ arcana/xnat/deploy/command.py
@@ -208,7 +208,7 @@ class XnatCommand(ContainerCommand):
                     "via-wrapup-command": None,
                     "as-a-child-of": "SESSION",
                     "type": "Resource",
-                    "label": output.name,
+                    "label": output.name,  # Shame that this is fixed, would be good to append the "dataset_name" to it as we do in the API put
                     "format": output.datatype.mime_like,
                 }
             )
